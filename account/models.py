dd5a9d17bacbce41a4927ff6354480eeb01a694c
Mokhirjon <Mokhirjon@test.com>
2023-10-17 17:31:28 +0500

    'smth_changed'

--- account/models.py
+++ account/models.py
@@ -7,6 +7,7 @@ class Customuser(AbstractUser):
     ROLE_CHOICES=(
         (1,'admin'),
         (2,'buyer')
+
     )
     role = models.SmallIntegerField(default=1,choices=ROLE_CHOICES)
     phone = models.CharField(default='',max_length=20)
--- config/permissions.py
+++ config/permissions.py
@@ -7,19 +7,10 @@ class AdminPermission(permissions.BasePermission):
             return False
         return request.user.role == 1
 
-    def has_object_permission(self, request, view, obj):
-        if not request.user.is_authenticated:
-            return False
-        return request.user.role == 1
-
-
 class BuyerPermission(permissions.BasePermission):
     def has_permission(self, request, view):
         if not request.user.is_authenticated:
             return False
-        return request.user.role == 1 or request.user.role == 2
+        return  request.user.role == 2
 
-    def has_object_permission(self, request, view, obj):
-        if not request.user.is_authenticated:
-            return False
-        return request.user.role == 1 or request.user.role == 2
+
