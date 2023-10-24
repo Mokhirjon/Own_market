8296d64c8a536b96b116df1122c08f5d3f029019
Mokhirjon <Mokhirjon@test.com>
2023-10-24 14:25:51 +0500

    ghjkl

--- Sold_Things/models.py
+++ Sold_Things/models.py
@@ -1,4 +1,6 @@
 from django.db import models
+from account.models import Customuser
+
 # Create your models here.
 
 class Sold_out (models.Model):
@@ -6,7 +8,7 @@ class Sold_out (models.Model):
     sold_clothes = models.TextField()
     sold_pants= models.TextField()
     sold_shoes = models.TextField()
-
+    admin = models.ForeignKey(Customuser,on_delete=models.CASCADE,null=True)
     def __str__(self):
         return self.sold_things
     class Meta:
--- Sold_Things/urls.py
+++ Sold_Things/urls.py
@@ -3,7 +3,7 @@ from .views import (Sold_outdeleteapieview,Sold_outdetailapieview,
                     Sold_outcreateapieview,Sold_outupdateapieview,Sold_outListapieview)
 
 urlpatterns = [
-    path('',Sold_outListapieview.as_view()),
+    path('list',Sold_outListapieview.as_view()),
     path('update/<int:pk>/',Sold_outupdateapieview.as_view()),
     path('create/',Sold_outcreateapieview.as_view()),
     path('delete/<int:pk>/',Sold_outdeleteapieview.as_view()),
