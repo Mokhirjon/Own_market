84c5235e0c7aea8d9e0a95bc5ff7ec907ec516d7
Mokhirjon <Mokhirjon@test.com>
2023-10-26 15:11:14 +0500

    mistakes

--- Clothes/serializers.py
+++ Clothes/serializers.py
@@ -4,5 +4,5 @@ from .models import Clothes
 class ClothesSerializers(serializers.ModelSerializer):
     class Meta:
         model = Clothes
-        fields = ("shoes", 'image', 'price', 'brand', 'about_shoes', 'colour', 'size', 'year_created', 'made_in_from',
+        fields = ("clothes", 'image', 'price', 'brand', 'about_clothes', 'colour', 'size', 'year_created', 'made_in_from',
                   'if_sold', 'for_which_season', 'Contact_with_admin')
